98c108420c499b056fb69fa0b8a569c27aa0a991
amila1998 <amiladevin@gmail.com>
2023-10-03 19:54:21 +0530

    facebook oAuth

--- mern-estate-main/api/routes/auth.route.js
+++ mern-estate-main/api/routes/auth.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { google, signOut, signin, signup } from '../controllers/auth.controller.js';
+import { facebook, google, signOut, signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post('/google', google);
+router.post('/facebook', facebook);
 router.get('/signout', signOut)
 
 export default router;
--- mern-estate-main/client/src/components/OAuth.jsx
+++ mern-estate-main/client/src/components/OAuth.jsx
@@ -3,10 +3,14 @@ import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
 import { useNavigate } from 'react-router-dom';
+import { LoginSocialFacebook } from "reactjs-social-login";
+import { FacebookLoginButton } from "react-social-login-buttons";
+import { useState } from 'react';
 
 export default function OAuth() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [profile, setProfile] = useState(null);
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -32,13 +36,50 @@ export default function OAuth() {
       console.log('could not sign in with google', error);
     }
   };
+
+  const handleFaceBookLoging = async()=>{
+    try {
+      const res = await fetch("/api/auth/facebook", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: profile.name,
+          email: profile.email,
+          photo: profile.picture.data.url,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      console.log("🚀 ~ file: OAuth.jsx:44 ~ handleFaceBookLoging ~ error:", error)
+    }
+  }
+
   return (
-    <button
-      onClick={handleGoogleClick}
-      type='button'
-      className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
-    >
-      Continue with google
-    </button>
+    <>
+      <button
+        onClick={handleGoogleClick}
+        type="button"
+        className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
+      >
+        Continue with google
+      </button>
+      <LoginSocialFacebook
+        appId="1380227986205378"
+        onResolve={(response) => {
+          // console.log(response);
+          setProfile(response.data);
+          handleFaceBookLoging();
+        }}
+        onReject={(error) => {
+          console.log(error);
+        }}
+      >
+        <FacebookLoginButton />
+      </LoginSocialFacebook>
+    </>
   );
 }
